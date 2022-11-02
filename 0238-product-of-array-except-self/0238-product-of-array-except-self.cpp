8bfa682b6e5c7dadaa1420aebfa429a3af2ff808
ARCHIT KUMAR AWASTHI <101095449+architak47@users.noreply.github.com>
2022-11-03 01:36:42 +0530

    Time: 39 ms (74.01%), Space: 23.9 MB (87.76%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,16 +3,14 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = size(nums);
         int leftmul = 1;
+        int rightmul = 1;
         vector<int> ans(n,1);
 
-        for(int i = 0; i < n; i++){
-            ans.at(i) = leftmul;
+        for(int i = 0, j = n-1; i < n; i++,j--){
+            ans.at(i) = leftmul * ans.at(i);
             leftmul = leftmul * nums[i];
-        }
-        int rightmul = 1;
-        for(int i = n-1; i >=0; i--){
-            ans[i] = rightmul * ans[i];
-            rightmul = rightmul * nums[i];
+            ans.at(j) = rightmul * ans.at(j);
+            rightmul = rightmul * nums[j];
         }
         return ans;
     }
